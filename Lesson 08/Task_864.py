981e185886aeb7a964895d4cfd7f2c41b268a256
Konstantin Syrovatsky <konstantin.syrovatsky@dptechnology.com>
2017-12-25 23:44:23 +0700

    1

---
+++ Lesson 08/Task_864.py
@@ -0,0 +1,47 @@
+"""
+Cipher
+"""
+
+
+
+def rotator_plus90(grille):
+    grille_90 = tuple(zip(*grille[::-1]))
+
+    return grille_90
+
+def make_string(grille):
+    pass
+
+
+def decrypt(crypted_text, cipher_grille):
+    cleartext = []
+    for i in range(3):
+        crypted_row = ''
+        cipher_row = ''
+        crypted_row = crypted_row.join(crypted_text)
+        cipher_row  = cipher_row.join(cipher_grille)
+        zipped_row = tuple(zip(cipher_row, crypted_row))
+        part_clear_text = [element[1] for element in zipped_row if element[0] is 'X' ]
+        cleartext.append(part_clear_text)
+        cipher_grille = rotator_plus90(cipher_grille)
+    return cleartext
+
+def main():
+    cipher_grille = ('X...',
+                    '..X.',
+                    'X..X',
+                    '....')
+
+    crypted_text = ('itdf',
+                    'gdce',
+                    'aton',
+                    'qrdi')
+
+    cipher_grille = rotator_plus90(cipher_grille)
+    print(cipher_grille)
+    
+    #clear_text = decryptor(crypted_text, cipher_grille)
+    #print(clear_text)
+
+if __name__ == '__main__':
+    main()
